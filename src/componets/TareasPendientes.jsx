4d389fea39b24998914c3dcc92dfc62f1ab7f273
Alane <alanenmexico12@gmail.com>
2023-07-01 12:15:29 -0600

    Cambios

--- src/componets/TareasPendientes.jsx
+++ src/componets/TareasPendientes.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Nav from './Nav';
 import ProductTable from './Mapita';
 import Calendario from './Mapa'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/Login.css';
+
+//import './css/Login.css';
 const Tareas = () => {
     return (
-        <div className="container-fluid contsfondo">
+        <div className="container-fluid ">
             <div className="row">
                 <h1 className='text-white'>Tareas Pendientes</h1>
                 <div class="card" style={{width:" 18rem;"}}>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Entrega Taquera</li>
+                        <li class="list-group-item">Entrega Taquera </li>
                         <li class="list-group-item">Produccion cazo</li>
                         <li class="list-group-item">Creacion Refrigerador</li>
                     </ul>
